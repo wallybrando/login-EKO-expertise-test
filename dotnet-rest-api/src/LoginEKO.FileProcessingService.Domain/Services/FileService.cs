93034da613e54a0ffc01a7b888e2fa17d5944c49
Marko Stojkovic <marko.stojkovic@mera.com>
2025-09-19 17:41:39 +0200

    commit

--- dotnet-rest-api/src/LoginEKO.FileProcessingService.Domain/Services/FileService.cs
+++ dotnet-rest-api/src/LoginEKO.FileProcessingService.Domain/Services/FileService.cs
@@ -1,8 +1,10 @@
 ﻿using LoginEKO.FileProcessingService.Domain.Exceptions;
+using LoginEKO.FileProcessingService.Domain.Extensions;
 using LoginEKO.FileProcessingService.Domain.Interfaces;
 using LoginEKO.FileProcessingService.Domain.Interfaces.Repositories;
 using LoginEKO.FileProcessingService.Domain.Interfaces.Services;
 using LoginEKO.FileProcessingService.Domain.Models;
+using LoginEKO.FileProcessingService.Domain.Models.Entities;
 using LoginEKO.FileProcessingService.Domain.Models.Enums;
 using LoginEKO.FileProcessingService.Domain.Utils;
 using LoginEKO.FileProcessingService.Domain.Validators;
@@ -14,71 +16,67 @@ namespace LoginEKO.FileProcessingService.Domain.Services
     public class FileService : IFileService
     {
         private readonly IServiceProvider _serviceProvider;
-        private readonly IFileRepository _fileMetadataRepository;
+        private readonly IFileMetadataRepository _fileMetadataRepository;
         private readonly ILogger<FileService> _logger;
 
-        public FileService(IServiceProvider serviceProvider, IFileRepository fileMetadataRepository, ILogger<FileService> logger)
+        public FileService(IServiceProvider serviceProvider, IFileMetadataRepository fileMetadataRepository, ILogger<FileService> logger)
         {
-            _serviceProvider = serviceProvider;
+             _serviceProvider = serviceProvider;
             _fileMetadataRepository = fileMetadataRepository;
             _logger = logger;
         }
 
+        /// <summary> Import telemetry for agro vehicle with ETL (extract-transform-load) approach </summary>
         public async Task<int> ImportVehicleTelemetryAsync(FileMetadata file, CancellationToken token = default)
         {
+            /*** Validate input parameters ****************************/
             var fileHashBytes = MD5Validator.ComputeHash(file.BinaryObject);
             var fileHash = MD5Validator.CreateHashStringFromHashBytes(fileHashBytes);
 
-            var fileDb = await _fileMetadataRepository.GetByMD5HashAsync(fileHash, token);
-            if (fileDb != null)
+            var fileExists = await _fileMetadataRepository.ExistsByMD5HashAsync(fileHash, token);
+            if (fileExists)
             {
                 _logger.LogError("File has been corrupted");
                 throw new FileValidationException("File has been corrupted");
             }
 
-            var vehicleType = FileManager.GetVehicleTypeFromFilename(file.Filename);
+            var vehicleType = FileHandler.GetVehicleTypeFromFilename(file.Filename);
             if (vehicleType == VehicleType.UNKNOWN)
             {
                 _logger.LogError("Filename format is invalid");
                 throw new FileValidationException("Name of file is invalid");
             }
 
-            var fileType = FileManager.GetFileTypeFromExtension(file.Extension);
+            var fileType = FileHandler.GetFileTypeFromExtension(file.Extension);
             if (fileType == FileType.UNKNOWN)
             {
                 _logger.LogError("File type is not supported");
                 throw new FileValidationException("File type is not supported");
             }
 
-            var fileExtractor = _serviceProvider.GetServices<IFileExtractor>()
-                                                .FirstOrDefault(x => x.Type == fileType)
-                                                ??
-                                                throw new ArgumentNullException("fileExtractor");
+            var fileExtractor = _serviceProvider.GetRequiredKeyedService<IFileExtractor>(fileType.GetDescription());
 
-            var vehicleDataParser = _serviceProvider.GetServices<IVehicleDataParser>()
-                                                           .FirstOrDefault(x => x.Type == vehicleType)
-                                                           ??
-                                                           throw new ArgumentNullException("vehicleDataParser");
-
-            file.MD5Hash = fileHash;
+            /*** Extract telemetry data from file **************************************/
             var extractedData = await fileExtractor.ExtractDataAsync(file.File, token);
             if (!extractedData.Any())
-                return 0;
+            {
+                throw new FileValidationException("Document cannot be empty");
+            }
 
-            var telemetries = vehicleDataParser.TransformVehicleData(extractedData);
+            /*** Transform telemetry data **********************************************/
+            var telemetryParser = _serviceProvider.GetRequiredKeyedService<IVehicleTelemetryParser>(vehicleType.GetDescription());
+            var telemetries = telemetryParser.ParseAgroVehicleTelemetry(extractedData);
             switch (vehicleType)
             {
-                case VehicleType.TRACTOR: file.TractorTelemetries = (ICollection<TractorTelemetry>) telemetries;
+                case VehicleType.TRACTOR: file.TractorTelemetries = (List<TractorTelemetry>)telemetries;
                     break;
-                case VehicleType.COMBINE: file.CombineTelemetries = (ICollection<CombineTelemetry>)telemetries;
+                case VehicleType.COMBINE: file.CombineTelemetries = (List<CombineTelemetry>)telemetries;
                     break;
             }
 
-            // minus 1 because one affected row is for filemetadata insert
-            var dbRowsAffected = await _fileMetadataRepository.ImportFileAsync(file, token) - 1;
-
-            if (dbRowsAffected <= 0)
-                return 0;
+            /*** Load telemety in Database *********************************************/
+            file.MD5Hash = fileHash;
+            var dbRowsAffected = await _fileMetadataRepository.ImportFileMetadataAsync(file, token) - 1; // minus 1 because one affected row is for filemetadata insert
 
             return dbRowsAffected;
         }
